67a2fdbc1052ff598990370b61dfdccfb23ded6e
clayamore <anmer.seif>
2023-07-18 15:15:54 +0200

    MIT LICENCE fix

--- include/SigScan.h
+++ include/SigScan.h
@@ -1,5 +1,5 @@
 /*
-Copyright (c) <year> <copyright holders>
+Copyright (c) 2023 tremwil@gmail.com
 
 Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation files (the
